830de1b6819a82d48f0b92812cf8a3cb2562320f
weihubeast <weihu641884200@163.com>
2019-05-21 16:27:10 +0800

    springcloud 服务注册与发现【一】

---
+++ service-ribbon/src/main/java/com/zou/serviceribbon/controller/HelloControler.java
@@ -0,0 +1,24 @@
+package com.zou.serviceribbon.controller;
+
+import com.zou.serviceribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: WH
+ * @Date: 2019/5/15 14:22
+ * @Version 1.0
+ */
+@RestController
+public class HelloControler {
+
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping(value = "/hi")
+    public String hi(@RequestParam String name) {
+        return helloService.hiService( name );
+    }
+}
